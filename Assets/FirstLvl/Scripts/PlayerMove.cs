c13c687d1372b5010c9584d41a8be22ebf84856c
vaijer1 <66016637+vaijer1@users.noreply.github.com>
2020-06-05 00:10:47 +0300

    Add --all

---
+++ Assets/FirstLvl/Scripts/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+    public float MoveInput;
+    
+    public bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        MoveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(MoveInput * Speed, rb.velocity.y);
+    }
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+        if(isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * JumpForce;
+        }
+    }
+
+}
