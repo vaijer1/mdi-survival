75739bdae705a0345408259716e43cc5bb235388
Ivan Grigorov <iigrigorov@edu.hse.ru>
2020-06-05 20:56:27 +0400

    New English level
    Added character, platform and some simple movement mechanism

---
+++ Assets/Flex/mainHero.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainHero : MonoBehaviour
+{
+    public float speed = 10f;
+    Rigidbody2D physics;
+
+    void Start()
+    {
+        physics = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        physics.MovePosition(physics.position + Vector2.right * x * speed * Time.deltaTime);
+    }
+}
