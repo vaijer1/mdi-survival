2a854f21ddf6cac40593c2fff53c145d64ca325d
vaijer1 <66016637+vaijer1@users.noreply.github.com>
2020-06-05 10:50:23 +0300

    Add all

--- Assets/FirstLvl/Scripts/PlayerMove.cs
+++ Assets/FirstLvl/Scripts/PlayerMove.cs
@@ -2,32 +2,73 @@
 
 public class PlayerMove : MonoBehaviour
 {
-    public float Speed;
-    public float JumpForce;
-    public float MoveInput;
-    
+    public float speed;
+    public float jumpForce;
+    public float moveInput;
+
+    private bool facingRight = true;
+
+
     public bool isGrounded;
     public Transform feetPos;
     public float checkRadius;
     public LayerMask whatIsGround;
 
+    private Animator anim;
+
+
     private Rigidbody2D rb;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     private void FixedUpdate()
     {
-        MoveInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(MoveInput * Speed, rb.velocity.y);
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        if(facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if(facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+        if(moveInput == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            anim.SetBool("isRunning", true);
+        }
+        if (isGrounded == true)
+        {
+            anim.SetBool("inAir", false);
+        }
+        else
+        {
+            anim.SetBool("inAir", true);
+        }
     }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
     private void Update()
     {
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
         if(isGrounded == true && Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = Vector2.up * JumpForce;
+            rb.velocity = Vector2.up * jumpForce;
         }
     }
+
 
 }
