c6727280c9d01ef64bf3f4b49873275dbd84c425
vaijer1 <66016637+vaijer1@users.noreply.github.com>
2020-06-07 02:56:28 +0300

    Add --all

---
+++ Assets/FirstLvl/Scripts/Dialogues/DialogueManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text dialogueText;
+    public Text nameText;
+
+    public Animator BoxAnim;
+    public Animator startAnim;
+
+    private Queue<string> sentences;
+
+    private void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        BoxAnim.SetBool("boxOpen", true);
+        startAnim.SetBool("startOpen", false);
+
+        nameText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    public void EndDialogue()
+    {
+        BoxAnim.SetBool("boxOpen", false);
+    }
+}
