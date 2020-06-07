12c74da31abbc022c978efbe4a0e9e3ce4f078c9
vaijer1 <66016637+vaijer1@users.noreply.github.com>
2020-06-07 08:11:15 +0300

    Add --all

---
+++ Assets/FirstLvl/Scripts/SwapImage.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SwapImage : MonoBehaviour
+{
+    public Sprite pic1;
+    public Sprite pic2;
+
+    private Image im;
+    void Start()
+    {
+        im = GetComponent<Image>();
+        im.sprite = pic1;
+    }
+    public void Swap()
+    {
+        if (im.sprite == pic1)
+        {
+            im.sprite = pic2;
+            return;
+        }
+
+        if (im.sprite == pic2)
+        {
+            im.sprite = pic1;
+            return;
+        }
+    }
+}
