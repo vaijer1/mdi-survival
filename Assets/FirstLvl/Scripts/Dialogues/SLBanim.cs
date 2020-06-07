ab07f91f5e5e21ed72c10533d329fd4b03888ca0
vaijer1 <66016637+vaijer1@users.noreply.github.com>
2020-06-07 23:02:24 +0300

    Add --all

---
+++ Assets/FirstLvl/Scripts/Dialogues/SLBanim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SLBanim : MonoBehaviour
+{
+    public Animator startAnim;
+    public DialogueManager dm;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        startAnim.SetBool("SLBopen", true);
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        startAnim.SetBool("SLBopen", false);
+        dm.EndDialogue();
+    }
+
+}
