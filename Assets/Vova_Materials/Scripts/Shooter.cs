543fa648d37a39a564618d474fde4f9ed5b2bf68
mrbunom <mr.bunom@mail.ru>
2020-06-07 22:37:34 +0300

    ...

---
+++ Assets/Vova_Materials/Scripts/Shooter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform shotPoint;
+
+    private float timebtwShts;
+    public float startTimebtwShts;
+
+
+    void Update()
+    {
+        if (timebtwShts <= 0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Instantiate(bullet, shotPoint.position, transform.rotation);
+                timebtwShts = startTimebtwShts;
+            }
+        }
+        else
+        {
+            timebtwShts -= Time.deltaTime;
+        }
+    }
+}
